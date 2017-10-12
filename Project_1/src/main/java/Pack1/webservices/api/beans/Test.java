7da2e30c851e6bba626f55e95ace7391e590f22a
Varun <varjk30@gmail.com>
2017-10-12 12:36:17 +0530

    Project Add commit

---
+++ Project_1/src/main/java/Pack1/webservices/api/beans/Test.java
@@ -0,0 +1,15 @@
+package Pack1.webservices.api.beans;
+
+public class Test {
+
+	private final String name;
+
+	public Test(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
