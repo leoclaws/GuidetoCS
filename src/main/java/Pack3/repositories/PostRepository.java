33e712c2b0c37b3563ecdced123dbee08d8ef2a5
Varun <varjk30@gmail.com>
2017-11-13 23:48:11 +0530

    Nov 13 edit

--- src/main/java/Pack3/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package Pack3.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Pack3.entities.Post;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
